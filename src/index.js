00c59207a7a35cd6ce35698b7c49eab585a6ec83
Nuwan94 <nuwansalawatta@gmail.com>
2020-08-16 05:36:35 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -6,19 +6,19 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
 b2cauth.initialize({
-  instance: "https://login.microsoftonline.com/tfp/",
-  tenant: "uoksetu.onmicrosoft.com",
-  signInPolicy: "B2C_1_react_signup",
-  applicationId: "1278766e-5c26-4964-842d-6f5f50494992",
-  cacheLocation: "sessionStorage",
-  scopes: ["https://uoksetu.onmicrosoft.com/api/user_impersonation"],
-  redirectUri: window.location.origin,
-  postLogoutRedirectUri: window.location.origin,
+    instance: "https://login.microsoftonline.com/tfp/",
+    tenant: "uoksetu.onmicrosoft.com",
+    signInPolicy: "B2C_1_react_signup",
+    applicationId: "1278766e-5c26-4964-842d-6f5f50494992",
+    cacheLocation: "sessionStorage",
+    scopes: ["https://uoksetu.onmicrosoft.com/api/user_impersonation"],
+    redirectUri: window.location.origin,
+    postLogoutRedirectUri: window.location.origin,
 });
 
 b2cauth.run(() => {
-  ReactDOM.render(<App />, document.getElementById("root"));
-  serviceWorker.unregister();
+    ReactDOM.render( < App / > , document.getElementById("root"));
+    serviceWorker.unregister();
 });
 
 serviceWorker.unregister();
